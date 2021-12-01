bbef63abef313a78762da454b140bedf32627c14
kiyo0216 <akakiyo0216@gmail.com>
2021-12-01 11:24:52 -0800

    added AWS cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-1_OTpdSWK2x', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4p4m3cfma1aou5dq4ivrdtcmqc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
